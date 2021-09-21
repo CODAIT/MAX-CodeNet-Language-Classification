7cdf66c6ff0d148a8012ef64c09808d8e05b8482
Karthik Muthuraman <karthik.muthuraman@ibm.com>
2021-09-20 19:17:32 -0700

    add test case

--- tests/test.py
+++ tests/test.py
@@ -28,7 +28,7 @@ def test_swagger():
 
     json = r.json()
     assert 'swagger' in json
-    assert json.get('info') and json.get('info').get('title') == 'Model Asset Exchange Server'
+    assert json.get('info') and json.get('info').get('title') == 'CodeNet Language Classification Model'
 
 
 def test_metadata():
@@ -39,20 +39,21 @@ def test_metadata():
     assert r.status_code == 200
 
     metadata = r.json()
+    assert metadata['name'] == 'CodeNet Language Classification'
 
 
 def test_response():
     model_endpoint = 'http://localhost:5000/model/predict'
-    file_path = 'samples/s034703969.c'
+    file_path = 'samples/s553234398.py'
 
-    with open(file_path, 'rb') as file:
-        file_form = {'file': (file_path, file, 'file')}
-        r = requests.post(url=model_endpoint, files=file_form)
+    files = {'file': ('s553234398.py', open(file_path, 'rb'), 'application/json')}
+    r = requests.post(url=model_endpoint, files=files)
 
     assert r.status_code == 200
     response = r.json()
 
     assert response['status'] == 'ok'
+    assert response['predictions'][0]['language'] == 'Python'
 
     # add sanity checks here
 
